ac841af3ea29bdbd374b3389b1c613ba5fd007f3
RollingGecko <RollingGecko@users.noreply.github.com>
2016-01-11 12:54:33 +0100

    Fix issue missing Config.h as part of the example.
    Add Config.h to the example as it is required for VescUart.h.

---
+++ examples/VescUartSample/Config.h
@@ -0,0 +1,24 @@
+/*
+Copyright 2015 - 2017 Andreas Chaitidis Andreas.Chaitidis@gmail.com
+This program is free software : you can redistribute it and / or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program.If not, see <http://www.gnu.org/licenses/>.
+*/
+
+//The Config.h is a file, that I use in other programs usualy. So I define also the serial ports there. If you don't want to
+//use it, just comment the include statement in the VescUart.h out.
+
+#ifndef _CONFIG_h
+#define _CONFIG_h
+
+  #define SERIALIO Serial1  
+  #define DEBUGSERIAL Serial
+
+#endif
